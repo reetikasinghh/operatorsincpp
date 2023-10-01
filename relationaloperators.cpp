6f078f7218c95e6b58e6fb3e5e20a612dbdc386d
reetikasinghh <139485681+reetikasinghh@users.noreply.github.com>
2023-10-01 13:10:29 +0530

    Update relationaloperators.cpp

--- relationaloperators.cpp
+++ relationaloperators.cpp
@@ -38,3 +38,23 @@ int main() {
 /*The two numbers are not equal
 The first number is less than the second number
 The first number is less than or equal to the second number*/
+
+
+/*WAP TO FIND WHETHER THE NUMBER IS ODD EVEN OR ZERO*/
+#include<iostream>
+using namespace std;
+int number;
+int main()
+{
+cout<<"enter the first number:";
+cin>>number;
+if (number%2==0)
+{cout<<"the number is even";}
+else
+{cout <<"the number is odd";}
+}
+/* OUTPUT: enter the first number:56
+the number is even.
+enter the first number:23
+the number is odd*/
+
